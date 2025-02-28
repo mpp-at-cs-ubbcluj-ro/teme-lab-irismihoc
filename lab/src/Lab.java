02683f91abbe5a37671801e178220595a8fa300d
Iris <iris.dm04@gmail.com>
2025-02-28 12:53:43 +0200

    tema 1

--- lab/src/Lab.java
+++ lab/src/Lab.java
@@ -3,5 +3,6 @@ public class Lab {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.println("This is a lab project.");
     }
 }
